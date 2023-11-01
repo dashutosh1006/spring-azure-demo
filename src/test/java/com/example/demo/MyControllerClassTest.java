38f7bfd53dc54e1ca83312f954f3dbf6ee7e0657
dashutosh1006 <k.ashutosh1006@gmail.com>
2023-11-01 11:22:26 +0530

    .....

--- src/test/java/com/example/demo/MyControllerClassTest.java
+++ src/test/java/com/example/demo/MyControllerClassTest.java
@@ -1,19 +1,17 @@
-package com.example.demo;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MyControllerClassTest {
-
-	private MyControllerClass myControllerClass;
-
-	@Test
-	void getEmployee() {
-		String employee = myControllerClass.getEmployee();
-		assertEquals("Ashutosh | 26 | Java Developer", employee);
-	}
-
-}
+/*
+ * package com.example.demo;
+ * 
+ * import static org.junit.jupiter.api.Assertions.assertEquals;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class MyControllerClassTest {
+ * 
+ * private MyControllerClass myControllerClass;
+ * 
+ * @Test void getEmployee() { String employee = myControllerClass.getEmployee();
+ * assertEquals("Ashutosh | 26 | Java Developer", employee); }
+ * 
+ * }
+ */
